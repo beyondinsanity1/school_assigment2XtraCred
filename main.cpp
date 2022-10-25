9d1c4ee76794a921e89cd19af510f6c658bbfcc5
beyondinsanity <ernsonmez@gmail.com>
2022-10-25 15:40:12 -0400

    assigment 2 with structs for extra credit.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using namespace std;
+
+struct gameInfo {
+    string gameName;
+    int replayScore;
+    int graphicScore;
+};
+        
+gameInfo games [5] {};
+
+//function getGameName
+void getGameName(){
+    for (int i = 0; i < 5; ++i) {
+        cout << "enter a game name" << endl;
+        getline(cin,(games[i].gameName));
+    }
+    for (int i = 0; i < 5; ++i) {
+        cout << games[i].gameName << endl;
+    }
+}
+
+//function getReplayScores
+
+
+//function getGraphicScores
+
+
+//function getHighestReplay
+
+
+//function getHighestGraphic
+
+
+//function menu
+
+
+int main(){
+    getGameName();
 }
